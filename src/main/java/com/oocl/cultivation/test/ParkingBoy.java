ce16835bafedf26fa5491b9fcf230e226c67b57a
Liufan666 <1663531467@qq.com>
2020-07-24 17:07:25 +0800

    [TDD Parking Lot] [Kevin] refactor: modify ParkingBoy.parking method

--- src/main/java/com/oocl/cultivation/test/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/test/ParkingBoy.java
@@ -2,7 +2,7 @@ package com.oocl.cultivation.test;
 
 public class ParkingBoy {
     public Ticket parking(Car car) {
-        return new Ticket("123456");
+        return new Ticket(car.getNumber());
     }
 
     public Car fetch(Ticket ticket) {
