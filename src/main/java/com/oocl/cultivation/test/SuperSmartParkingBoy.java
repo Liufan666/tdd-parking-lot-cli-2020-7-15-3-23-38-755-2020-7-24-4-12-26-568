61fd0490b32ab0873a0910e9b7008f72dd9e67a9
Liufan666 <1663531467@qq.com>
2020-07-26 21:37:51 +0800

    [TDD Parking Lot] [Kevin] feature: pass should_return_true_when_iscontaincar_after_parking_in_two_parkinglot_given_four_car_and_supersmartparkingboy

--- src/main/java/com/oocl/cultivation/test/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/test/SuperSmartParkingBoy.java
@@ -3,6 +3,26 @@ package com.oocl.cultivation.test;
 public class SuperSmartParkingBoy extends ParkingBoy{
     @Override
     public Ticket parking(Car car) {
-        return super.parking(car);
+        int indexOfMaxEmptyRate = 0;
+        for(int index=0;index<parkingLots.size();index++){
+            if(((parkingLots.get(indexOfMaxEmptyRate).getCapacity()-
+                    parkingLots.get(indexOfMaxEmptyRate).getParkingRoom().size())
+            *100/parkingLots.get(indexOfMaxEmptyRate).getCapacity())
+                    <((parkingLots.get(index).getCapacity()-
+                    parkingLots.get(index).getParkingRoom().size())
+                    *100/parkingLots.get(index).getCapacity())){
+                indexOfMaxEmptyRate = index;
+            }
+
+        }
+        if(parkingLots.get(indexOfMaxEmptyRate).isNoPosition()){
+            System.out.print("Not enough position.\n");
+            return null;
+        }
+        Ticket ticket = new Ticket(car.getNumber());
+        ticket.setParkingBoy(this);
+        parkingLots.get(indexOfMaxEmptyRate).addParkingRoom(ticket,car);
+        return ticket;
+
     }
 }
