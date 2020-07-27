fd007bf00a5195113e0afcc74b5bf1365b16f0a5
Liufan666 <1663531467@qq.com>
2020-07-27 17:05:05 +0800

    [TDD Parking Lot] [Kevin] refactor: replace veriable ParkingLot in SuperSmartParkingBoy.parking method

--- src/main/java/com/oocl/cultivation/test/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/test/SuperSmartParkingBoy.java
@@ -3,21 +3,22 @@ package com.oocl.cultivation.test;
 public class SuperSmartParkingBoy extends ParkingBoy{
     @Override
     public Ticket parking(Car car) {
-        int indexOfMaxEmptyRate = 0;
+        ParkingLot parkingLot = parkingLots.get(0);
         for(int index=0;index<parkingLots.size();index++){
-            if((parkingLots.get(indexOfMaxEmptyRate).getEmptyRate())
+            if((parkingLot.getEmptyRate())
                     <(parkingLots.get(index).getEmptyRate())){
-                indexOfMaxEmptyRate = index;
+                parkingLot = parkingLots.get(index);
             }
 
         }
-        if(parkingLots.get(indexOfMaxEmptyRate).isNoPosition()){
+
+        if(parkingLot.isNoPosition()){
             System.out.print("Not enough position.\n");
             return null;
         }
         Ticket ticket = new Ticket(car.getNumber());
         ticket.setParkingBoy(this);
-        parkingLots.get(indexOfMaxEmptyRate).addParkingRoom(ticket,car);
+        parkingLot.addParkingRoom(ticket,car);
         return ticket;
 
     }
